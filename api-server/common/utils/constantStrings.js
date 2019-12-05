87ebd67bed70c76db89dd6401d9b8e2c086ad947
Sonia Milanoi <soniamilanoi11@gmail.com>
2019-12-05 19:00:36 +0300

    Create constantStrings.js

---
+++ api-server/common/utils/constantStrings.js
@@ -0,0 +1,8 @@
+#{
+  "aboutUrl": "https://www.sitename.com/about",
+  "defaultProfileImage": "https://s3.amazonaws.com/sitename/siteimage-image-placeholder.png",
+  "forumUrl": "https://forum.freecodecamp.org",
+  "githubUrl": "https://github.com/finalsiteurl",
+  "RSA": "https://forum.sitename.org",
+  "homeURL": "https://www.sitename.com"
+}
